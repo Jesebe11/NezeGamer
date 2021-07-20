415573bde25cb0c3a720614e0bb06965c88c4ce2
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-20 13:22:04 -0400

    Was created palete colors and realizated some tests

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+    get_header();
+?>
+
+	<main id="primary" class="main">
+
+		<?php
+
+
+			get_template_part( 'template-parts/content-home');
+
+
+		?>
+
+	</main>
+
+<?php
+get_footer();
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+        colors:{
+          //layout
+          'nezegamer-red' : '#df163b',
+          'nezegamer-white-single' : '#fff' ,
+          'nezegamer-white-comple' : '#f8f9fa',
+          'nezegamer-gray' : '#6c757d' ,
+          'nezegamer-gray-light' : '#292e38' ,
+          'nezegamer-gray-dark' : '#181c23',
+
+          //labels or flags
+          'nezegamer-blue' : '#007bff',
+          'nezegamer-indigo' : '#6610f2',
+          'nezegamer-orange' : '#fd7e14',
+          'nezegamer-purple' : '#6f42c1',
+          'nezegamer-yellow' : '#ffc107',
+          'nezegamer-green' : '#28a745',
+          'nezegamer-teal' : '#20c997',
+          'nezegamer-cyan' : '#17a2b8',
+
+          //Alerts or Warnings
+          'nezegamer-primary' : '#007bff',
+          'nezegamer-success' : '#28a745',
+          'nezegamer-info' : '#17a2b8',
+          'nezegamer-warning' : '#ffc107',
+          'nezegamer-danger' : '#dc3545',
+        },
+      },
   },
   variants: {},
   plugins: [],
