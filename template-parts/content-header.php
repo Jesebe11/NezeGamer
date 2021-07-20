e43171dc86e1332a5eced27653f91aa79555361e
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-20 15:32:03 -0400

    Reordered directories css and php

--- template-parts/content-header.php
+++ template-parts/content-header.php
@@ -1,8 +1,10 @@
 <?php
-    get_template_part( 'template-parts/components/content/header/top-head' );
+    get_template_part( 'template-parts/components/common/header/top-head' );
 ?>
 
-<?php
-    get_template_part( 'template-parts/components/content/header/site-branding' );
-    get_template_part( 'template-parts/components/content/header/primary-menu' );
-?>
+<div class="navigation">
+    <?php
+        get_template_part( 'template-parts/components/common/header/site-branding' );
+        get_template_part( 'template-parts/components/common/header/primary-menu' );
+    ?>
+</div>
