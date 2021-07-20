efbbda0e62dad789ffa068f1c5dd71b5c18b7ce1
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-20 14:55:00 -0400

    Was created directories for styles header

--- template-parts/components/content/header/site-branding.php
+++ template-parts/components/content/header/site-branding.php
@@ -4,13 +4,8 @@
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 		?>
-			<h1 class="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-					else :
-				?>
-			<p class="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-					endif;
-				?>
+			<?php
+				endif;
+			?>
 	</div>
 </div>
--- template-parts/content-header.php
+++ template-parts/content-header.php
@@ -1,4 +1,8 @@
 <?php
-    get_template_part( 'template-parts/components/content/header/site-branding' );
     get_template_part( 'template-parts/components/content/header/top-head' );
+?>
+
+<?php
+    get_template_part( 'template-parts/components/content/header/site-branding' );
     get_template_part( 'template-parts/components/content/header/primary-menu' );
+?>
