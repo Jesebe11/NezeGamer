dfab17263b5767fda1a0ee94c8e9dcb1e4c8c5be
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-22 10:45:43 -0400

    Finished blosck coy

--- template-parts/components/common/footer/copy.php
+++ template-parts/components/common/footer/copy.php
@@ -1,2 +1,5 @@
 <?php get_template_part( 'template-parts/components/common/header/site-branding' ); ?>
-<p class="copyright"> Copyright 2021, BellNers.com | whit love</p>
+<div class="info">
+    <p class="copyright"> © 2021 <a href="#"> NezeGamer.com </a> Todos los Derechos Reservados.</p>
+    <p class="copyright"> Desarrollado por <a href="#"> BellNers.com </a></p>
+</div>
