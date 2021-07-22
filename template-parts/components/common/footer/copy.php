f4712c5b0ef7d420230fd270b759d775aa26fbb4
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-22 16:58:06 -0400

    Fixed some errors and was been enhanced footer, was created button top

--- template-parts/components/common/footer/copy.php
+++ template-parts/components/common/footer/copy.php
@@ -1,4 +1,3 @@
-<?php get_template_part( 'template-parts/components/common/header/site-branding' ); ?>
 <div class="info">
     <p class="copyright"> © 2021 <a href="#"> NezeGamer.com </a> Todos los Derechos Reservados.</p>
     <p class="copyright"> Desarrollado por <a href="#"> BellNers.com </a></p>
--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -1,10 +1,12 @@
 <div class="navs-footer">
     <?php
+        get_template_part( 'template-parts/components/common/footer/up' );
         get_template_part( 'template-parts/components/common/footer/navs-footer' );
-        ?>
+    ?>
 </div>
 <div class="copy">
     <?php
+        get_template_part( 'template-parts/components/common/header/site-branding' );
         get_template_part( 'template-parts/components/common/footer/copy' );
     ?>
 </div>
