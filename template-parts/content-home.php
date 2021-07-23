d467046c85e83fbee2319767f077cd136e688b26
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-23 10:31:15 -0400

    had been assigned some size of diferents brakpoint of div principal

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,5 +1,5 @@
 <div class="block-section">
     <?php
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => 'Servicios' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => 'Ultimas noticias' , 'permalink' => '  '] );
     ?>
 </div>
