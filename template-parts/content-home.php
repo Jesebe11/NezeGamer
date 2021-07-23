c62c01db5f73b566ff16960e3acbd14318bb64c3
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-23 10:37:06 -0400

    Was created directories for blocks home

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,5 +1,6 @@
 <div class="block-section">
     <?php
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => 'Ultimas noticias' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/content/home/bh-latest-news');
     ?>
 </div>
