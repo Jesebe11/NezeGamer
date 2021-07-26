e2c2e84030db5657d6b21a2e4c6b948251f5bbfe
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-26 15:39:52 -0400

    Was created all directories for block home comics

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -3,5 +3,8 @@
         get_template_part( 'template-parts/components/content/home/bh-destacado');
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Noticias</span> Marvel' , 'permalink' => '  '] );
         get_template_part( 'template-parts/components/content/home/bh-latest-news-marvel' /** , '' , ['class' => 'marvel' ] */ );
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Reviews</span> Comics' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/content/home/bh-comics' /** , '' , ['class' => 'marvel' ] */ );
+
     ?>
 </div>
