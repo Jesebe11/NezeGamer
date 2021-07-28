6de366e8a9d22c2782a4b6fa3e998b7eeec741c3
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-28 16:50:21 -0400

    Fixed some styles and ajusted query for block latest news into home

--- template-parts/components/content/home/bh-latest-news.php
+++ template-parts/components/content/home/bh-latest-news.php
@@ -5,8 +5,8 @@ $args = [
     'post_status' => 'publish',
     'orderby' => 'date',
     'order' => 'DESC',
-    'posts_per_page' => 4,
-    'category_name' => 'marvel'
+    'posts_per_page' => 6,
+    'category_name' => 'noticias'
 ];
 
 $the_query = new WP_Query( $args);
@@ -19,8 +19,16 @@ $the_query = new WP_Query( $args);
                 $title = get_the_title();
                 $author = get_the_author();
                 $date = get_the_date();
+                $categories = get_the_category();
             ?>
                 <article class="content">
+                    <ul class="dest__card__tags tags tags--news dest__card__tags--news">
+                        <?php
+                        if ( ! empty( $categories ) ) {
+                            echo '<a class="tags tags--news" href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+                        }
+                        ?>
+                    </ul>
                     <div class="content-overlay"></div>
                     <?= get_the_post_thumbnail( $post->ID, 'medium', array('class' => 'content-image') ); ?>
                         <div class="content__info">
@@ -30,14 +38,6 @@ $the_query = new WP_Query( $args);
                                 <li class="meta__item meta__item--comments"> <?php echo get_comments_number($post->ID); ?> </li>
                             </ul>
                             <h2 class="content-title"><?= $title; ?></h2>
-                            <p class="tags">
-                                <?
-                                    $post_tags = get_the_tags();
-                                    if ( $post_tags ) {
-                                    echo $post_tags[1]->name;
-                                }
-                                ?>
-                            </p>
                         </div>
                         <div class="content-details fadeIn-bottom">
                             <div class="content-text">
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,19 +1,19 @@
 <div class="block-section">
     <?php
         get_template_part( 'template-parts/components/content/home/bh-destacado');
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Noticias</span>&nbsp Marvel' , 'permalink' => '  '] );
-            get_template_part( 'template-parts/components/content/home/bh-latest-news-marvel');
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Ultimas</span>&nbsp noticias' , 'permalink' => '  '] );
+            get_template_part( 'template-parts/components/content/home/bh-latest-news');
         /** Blocks */
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Guias</span> Variadas' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Guias</span>&nbsp Variadas' , 'permalink' => '  '] );
             get_template_part( 'template-parts/components/content/home/bh-guides');
         /** Blocks */
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Partidos</span>&nbsp y equipos' , 'permalink' => '  '] );
             get_template_part( 'template-parts/components/content/home/bh-matches');
         /** Blocks */
-        // get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Game</span>plays' , 'permalink' => '  '] );
-        //         get_template_part( 'template-parts/components/content/home/bh-gameplays');
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Game</span>plays' , 'permalink' => '  '] );
+                get_template_part( 'template-parts/components/content/home/bh-gameplays');
         /** Blocks */
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Wall</span>papers' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Asombrosos</span>&nbspwallpapers' , 'permalink' => '  '] );
             get_template_part( 'template-parts/components/content/home/bh-galleries');
     ?>
 </div>
