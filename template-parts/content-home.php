e985386e3fb82c886ea41cab8ffecc740749924b
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-28 16:10:55 -0400

    Was gived styles for block matches

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -1,13 +1,13 @@
 <div class="block-section">
     <?php
         get_template_part( 'template-parts/components/content/home/bh-destacado');
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Noticias</span> Marvel' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Noticias</span>&nbsp Marvel' , 'permalink' => '  '] );
             get_template_part( 'template-parts/components/content/home/bh-latest-news-marvel');
         /** Blocks */
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Guias</span> Variadas' , 'permalink' => '  '] );
             get_template_part( 'template-parts/components/content/home/bh-guides');
         /** Blocks */
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Part</span>idos' , 'permalink' => '  '] );
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Partidos</span>&nbsp y equipos' , 'permalink' => '  '] );
             get_template_part( 'template-parts/components/content/home/bh-matches');
         /** Blocks */
         // get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Game</span>plays' , 'permalink' => '  '] );
