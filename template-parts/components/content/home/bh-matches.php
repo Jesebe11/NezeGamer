38c140d345f72c0e44bd3e50d3b5b6917f887161
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-28 13:42:42 -0400

    fixed problems with last commit and was added a block for matches

---
+++ template-parts/components/content/home/bh-matches.php
@@ -0,0 +1,5 @@
+<section class="matches">
+    <?php if( is_active_sidebar( 'sidebar-matches' ) ) {
+            dynamic_sidebar( 'sidebar-matches' );
+    } ?>
+</section>
--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -2,12 +2,18 @@
     <?php
         get_template_part( 'template-parts/components/content/home/bh-destacado');
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Noticias</span> Marvel' , 'permalink' => '  '] );
-        get_template_part( 'template-parts/components/content/home/bh-latest-news-marvel' /** , '' , ['class' => 'marvel' ] */ );
+            get_template_part( 'template-parts/components/content/home/bh-latest-news-marvel');
+        /** Blocks */
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Guias</span> Variadas' , 'permalink' => '  '] );
-        get_template_part( 'template-parts/components/content/home/bh-guides' /** , '' , ['class' => 'marvel' ] */ );
+            get_template_part( 'template-parts/components/content/home/bh-guides');
+        /** Blocks */
+        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Part</span>idos' , 'permalink' => '  '] );
+            get_template_part( 'template-parts/components/content/home/bh-matches');
+        /** Blocks */
+        // get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Game</span>plays' , 'permalink' => '  '] );
+        //         get_template_part( 'template-parts/components/content/home/bh-gameplays');
+        /** Blocks */
         get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Wall</span>papers' , 'permalink' => '  '] );
-        get_template_part( 'template-parts/components/content/home/bh-galleries' /** , '' , ['class' => 'marvel' ] */ );
-        get_template_part( 'template-parts/components/common/titles/titles' , '' , ['title' => '<span>Game</span>plays' , 'permalink' => '  '] );
-        get_template_part( 'template-parts/components/content/home/bh-gameplays' /** , '' , ['class' => 'marvel' ] */ );
+            get_template_part( 'template-parts/components/content/home/bh-galleries');
     ?>
 </div>
