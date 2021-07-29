2ea45131ba54690a5e352eba664f237742951f20
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-29 16:44:52 -0400

    Finished about us pus page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -3,10 +3,10 @@
 			get_template_part( 'template-parts/components/common/titles/titles' );
             if ( is_page( array( 'sobre-nosotros' ) ) ) {
                 get_template_part( 'template-parts/components/common/into-content/content', '  ',
-                ['class' => 'About Us' ] );
+                ['class' => 'contact-us' ] );
             } else {
                 get_template_part( 'template-parts/components/common/into-content/content' , '  ',
-                ['class' => 'content' ] );
+                ['class' => 'content-general' ] );
             }
 
 		?>
