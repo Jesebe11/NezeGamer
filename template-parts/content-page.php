6867241aa4ff261a50febb989aae64c5cee9093f
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-07-29 17:10:43 -0400

    Finished page terms and conditions

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,7 +1,7 @@
 <div class="block-section">
         <?php
 			get_template_part( 'template-parts/components/common/titles/titles' );
-            if ( is_page( array( 'sobre-nosotros' ) ) ) {
+            if ( is_page( array( 'sobre-nosotros' , 'politica-de-privacidad' ) ) ) {
                 get_template_part( 'template-parts/components/common/into-content/content', '  ',
                 ['class' => 'contact-us' ] );
             } else {
