b5ccd9372f701c5e3caa74f083dab8c4ac9f37e0
Maximiliano Fernández <mfernandez@telesurtv.net>
2021-08-16 10:26:29 -0430

    Was separated directories for 404 page

---
+++ template-parts/components/content/page/404/area-404.php
@@ -0,0 +1,5 @@
+<div class="widgets-error">
+            <?php if( is_active_sidebar( 'sidebar-404' ) ) {
+                dynamic_sidebar( 'sidebar-404' );
+            } ?>
+    </div>
--- template-parts/content-404.php
+++ template-parts/content-404.php
@@ -1,8 +1,6 @@
 <section class="error">
-    <img src="https://i.postimg.cc/C1sbZ3Pm/error-1349562-1280.png" alt="">
-    <div class="widgets-error">
-            <?php if( is_active_sidebar( 'sidebar-404' ) ) {
-                dynamic_sidebar( 'sidebar-404' );
-            } ?>
-    </div>
+    <?php
+        get_template_part( 'template-parts/components/content/page/404/animation-404' );
+        get_template_part( 'template-parts/components/content/page/404/area-404' );
+    ?>
 </section>
