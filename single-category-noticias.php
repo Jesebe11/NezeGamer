e875841a69f34b5ba81460791d5c6b6e19664fb4
Maximiliano Fernández <mfernandez@telesurtv.net>
2021-08-30 14:53:09 -0430

    fixed some styles

--- single-category-noticias.php
+++ single-category-noticias.php
@@ -25,15 +25,14 @@ noticia
 				)
 			);
 
-			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
 
-		endwhile; // End of the loop.
+		endwhile;
 		?>
 
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_footer();
